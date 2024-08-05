0a9c6e45e9872132932f4f51a498edd3ec28c82b
akyadav4710 <163339150+akyadav4710@users.noreply.github.com>
2024-08-05 14:33:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const char = {};
+	for(const char of str){
+		if(charCount[char]){
+			charCount[char]++;
+		}
+		else {
+			charCount[char] = 1;
+		}
+	}
+
+	for(const char of str){
+		if(charCount[char]===1){
+			return char;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
